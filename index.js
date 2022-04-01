a963ed2d90004267c87c3743e94c586621116b93
Swapnil <swapnilrajharsh@gmail.com>
2022-04-01 14:56:18 +0530

    Checking /oauth2/authorize

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const { redirect } = require('express/lib/response');
 const app = express()
 var morgan     = require('morgan');
 
@@ -7,7 +8,16 @@ var port     = process.env.PORT || 8080;
 // configure app
 app.use(morgan('dev'));
 app.get('/', (req, res) => {
-    res.send('&#128516 Welcome to our XYZ POC product service API &#128516');
+    res.send('&#128516 Welcome to our Swapnil\'s POC product service API &#128516');
+})
+
+app.get('/oauth2/authorize', (req, res) => {
+    const redirect_uri = req.body.redirect_uri;
+    res.redirect(redirect_uri);
+})
+
+app.get('/oauth2/token', (req, res) => {
+    res.send('Please Sign in with your account');
 })
 
 app.listen(port);
